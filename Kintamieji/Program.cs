a21b1878e58bf64f7ee86943aa76929271cf678d
User <User@DESKTOP-EDHBGBR>
2023-10-14 10:34:57 +0300

    Add project files.

---
+++ Kintamieji/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ernestas";
+            string lastName = "Švedas";
+            int age = 29;
+            int height = 178;
+            int weight = 76;
+            string university = "Vilnius university";
+            double acad_group_code = 123456789;
+            int course = 2;
+            string study_name = "Neurobiology";
+            double credit_done = 90;
+
+        }
+    }
+}
