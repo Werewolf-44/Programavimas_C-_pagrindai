d72e9ec0c7ea39d4e43d4b9b5951175ddd64c076
Ernestas Švedas <ernestas.svedas@gmail.com>
2023-10-17 21:54:51 +0300

    Fourth task

---
+++ uzduotis04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uzduotis04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Susikurkite reikiamus kintamuosius užduočiai atlikti. Į ekraną išveskite: Automobilio {markė} {modelis} ({metai} m.) rida - {rida} km.
+             * Tarp {} nurodyti reikiami kintamieji, šiose vietose jų reikšmės turi būti įstatytos.*/
+            string marke = "Hyundai";
+            string modelis = "Ioniq";
+            int metai = 2020;
+            double rida = 33476;
+            Console.WriteLine($"Automobilio {marke} {modelis} ({metai} m.) rida - {rida} km.");
+        }
+    }
+}
