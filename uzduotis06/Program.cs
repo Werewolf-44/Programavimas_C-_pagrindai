aa4292b89f561625df913cabf8a6df57bae2a41e
Ernestas Švedas <ernestas.svedas@gmail.com>
2023-10-18 20:03:28 +0300

    Fixed sixth task

--- uzduotis06/Program.cs
+++ uzduotis06/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,26 +13,23 @@ namespace uzduotis06
         {
             //Išveskite pasirinkto skaičiaus daugybos lentelę.
             int number = 8;
-            Console.WriteLine(number * 1);
-            Console.WriteLine(number * 2);
-            Console.WriteLine(number * 3);
-            Console.WriteLine(number * 4);
-            Console.WriteLine(number * 5);
-            Console.WriteLine(number * 6);
-            Console.WriteLine(number * 7);
-            Console.WriteLine(number * 8);
-            Console.WriteLine(number * 9);
-            Console.WriteLine(number * 10);
-
-            // Arba for loop
-            for (int i = 1; i < 11; i++)
-            {
-                Console.WriteLine(number * i);
-            }
+            int daugiklis = 1; 
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
+            Console.WriteLine(number + " * " + daugiklis + " = " + number * daugiklis++);
 
             //Susikurkite sveikojo skaičiaus kintamąjį su dviženklio skaičiaus reikšme. Išveskite į ekraną šio skaičiaus skaitmenų sandaugą.
-            double num = 28;
-            Console.WriteLine(2 * 8);
+            int num = 28;
+            int tens = num / 10;
+            int ones = num % 10;
+            Console.WriteLine($"Sveikasis dviženklis skaičius yra {num}. Pirmas skaičius yra {tens}, antras skaičius yra {ones}. Jų sandauga yra " + tens * ones);
         }
     }
 }
